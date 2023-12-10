9c00ddaa56a8120d0ae037e22a92ec309da1b0f3
xomaxy <angelcellwall@gmail.com>
2023-12-10 14:56:28 -0600

    test at main

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<p>
+    This is a modification to se if the selected things works
+    
+</p>
